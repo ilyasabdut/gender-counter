38052d84b96555e9d62fa423448915e529d2e81a
ilyas abduttawab <ilyas@stickearn.com>
2024-04-05 19:16:46 +0700

    add basic auth

--- app/Supports/helpers.php
+++ app/Supports/helpers.php
@@ -5,13 +5,6 @@ use Illuminate\Pagination\LengthAwarePaginator;
 function api($message,$statusCode,$model = null) {
     $meta = [];
 
-    $response = [
-        'statusCode' => $statusCode,
-        'message'  => $message,
-        'data' => $model,
-        'meta' => $meta
-    ];
-    
     if($statusCode == 200){
          $meta = [
             'total' => $model->count()
@@ -22,10 +15,18 @@ function api($message,$statusCode,$model = null) {
                 'data', 'links', 'last_page_url', 'first_page_url', 'next_page_url', 'path', 'prev_page_url'
             ]);
             $model = $model->items();
-
         }
     }
-    return response()->json($response);
+
+    $response = [
+        'statusCode' => $statusCode,
+        'message'  => $message,
+        'data' => $model,
+        'meta' => $meta
+    ];
+
+
+    return response()->json($response,$statusCode);
 }
 
 function randomUser(){
